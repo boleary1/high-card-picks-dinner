fd9d4a5c8f29e78f66c6f8ed7383ca01546fd1fe
Tiffany Brand <brand2learn@gmail.com>
2020-07-28 15:37:57 -0400

    Finish basic game styling

--- design-assets/design.js
+++ design-assets/design.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
 
+    // Selectors for main divs - game, recipe search, meal history
+    const gameDiv = $('div#game-div')
+
+    // for testing only - hide and show different divs
+    const hideGameBtn = $('#hide-game')
+
+    function hideShow(element) {
+        element.toggleClass("hide");
+    }
+
+    hideGameBtn.on("click", function () {
+        hideShow(gameDiv);
+    })
+
     // Materialize Side Nav Menu for Mobile
     $('.sidenav').sidenav();
+
 
 
 
