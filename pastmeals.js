4546d6f014910fccdf17ba50b43177e18fdaa0a6
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-29 21:11:57 -0400

    Added another call to API using ID to get link and other info; Fixed save data to local storage.

--- pastmeals.js
+++ pastmeals.js
@@ -13,38 +13,53 @@ $(document).ready(function() {
 
     $('select').formSelect(); //From materialize - not sure if i need this.
 
-    // Gets info from local storage and stores it in the historyData variable.
+    // Gets info from local storage and stores it in the historyData variable if there is any available.
     function getHistory() {
-        historyData = JSON.parse(localStorage.getItem('historyKEY')); //|| [{Name:"No History", Date:"day", APIobj: "No history yet"}];
+        historyData = JSON.parse(localStorage.getItem('historyKEY')) || [{Name:"", Meal: "", Date:"", ImgURL: "", RecipeLink: ""}];
     };
 
     function populateHistory() {
         console.log(historyData)
-        historyData.forEach(function(object){
-            const historyDiv = $('<div>').addClass('history col xs12 s6 m4 l2');
-            const historyCard = $('<div>').addClass('card');
-            const cardImgDiv = $('<div>').addClass('card-img');
-            const img = $('<img>').attr('src', object.ImgURL).attr('alt', object.Meal).width('100%');
-            const textDiv = $('<div>').addClass('history-text card-content');
-            const mealName = $('<p>').addClass('meal-name card-title').text(object.Meal);
-            const whoPicked = $('<p>').addClass('picked-by').text(`${object.Name} picked on:`);
-            const date = $('<p>').addClass('date-picked').text(`${object.Date}`);
+        // If history data exists, create a card for each object and add the img, meal name, who picked it, and date
+        if (historyData[0].Name) {
+            historyData.forEach(function(object){
+                const historyDiv = $('<div>').addClass('history col xs12 s6 m4 l2');
+                const historyCard = $('<div>').addClass('card');
+                const cardImgDiv = $('<div>').addClass('card-img');
+               // const aLink = $('<a>').attr('href', object.RecipeLink).attr('target', '_blank');
+                const img = $('<img>').attr('src', object.ImgURL).attr('alt', object.Meal).attr('class', 'imgLink').width('100%');
+                const textDiv = $('<div>').addClass('history-text card-content');
+                const mealName = $('<p>').addClass('meal-name card-title').text(object.Meal);
+                const whoPicked = $('<p>').addClass('picked-by').text(`${object.Name} picked on:`);
+                const date = $('<p>').addClass('date-picked').text(`${object.Date}`);
 
-            textDiv.append(mealName, whoPicked, date);
-            cardImgDiv.append(img);
-            historyCard.append(cardImgDiv, textDiv);
-            historyDiv.append(historyCard);
-            historyContainer.append(historyDiv);
-        });
+                // Wraps the imag in the link... not working... instead, try adding an event listener
+                //img.wrap(aLink);
+                textDiv.append(mealName, whoPicked, date);
+                cardImgDiv.append(img.wrap($(`<a href="${object.RecipeLink}" target="_blank">`))); //this didn't work either
+                historyCard.append(cardImgDiv, textDiv);
+                historyDiv.append(historyCard);
+                historyContainer.append(historyDiv);
+                console.log(object.RecipeLink);
+            });
+        }
+
 
     };
 
     // Stores meal info in local storage
-    function saveMeal(name, meal, picURL) {
+    function saveMeal(name, meal, picURL, recipeLink) {
         const curDate = moment().format('MM/DD/YYYY');
-        const newObj = {Name: name, Meal: meal, Date: curDate, ImgURL: picURL};
+        const newObj = {Name: name, Meal: meal, Date: curDate, ImgURL: picURL, RecipeLink: recipeLink};
         console.log(newObj);
-        historyData.push(newObj);
+        console.log(historyData[0].Name);
+        // If the name in the first history object is blank, set the historyData to the new object; otherwise, ad the new object to the array
+        if (!historyData[0].Name) {
+            historyData = [newObj];
+        } else {
+            historyData.push(newObj);
+        }
+
         console.log(historyData);
         localStorage.setItem('historyKEY', JSON.stringify(historyData));
     };
@@ -53,13 +68,19 @@ $(document).ready(function() {
     function searchCategory(category) {
         // Filter by category
         $.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}&apikey=1`)
-        .then(function(response){
+            .then(function(response){
             console.log(response);
-            console.log(response.meals[0].strMeal);
-            console.log(response.meals[0].strMealThumb); //URl for thumbnail
-            saveMeal(nameEl.val(), response.meals[0].strMeal, response.meals[0].strMealThumb);
+
+            //Use the meal id to make the call for the meal information (name, recipe link.
+            $.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${response.meals[0].idMeal}&apikey=1`)
+                .then(function(response){
+                    console.log(response);
+                    // Parameters: name of user; name of the meal; image of the meal, link to the recipe
+                    saveMeal(nameEl.val(), response.meals[0].strMeal, response.meals[0].strMealThumb, response.meals[0].strSource);
+                }
+            );
         });
-    }
+    };
 
 
 
@@ -78,8 +99,11 @@ $(document).ready(function() {
         event.preventDefault();
         console.log(categoryEl.val());
         searchCategory(categoryEl.val());
+    });
 
-    })
+    $("img.imgLink").on("click", function() {
+        
+    });
 
 
 
