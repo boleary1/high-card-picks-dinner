51134efbf867bd3b8bd230f16f73e48b8b92fff9
Tiffany Brand <brand2learn@gmail.com>
2020-07-29 20:11:38 -0400

    Basic search page design

--- design-assets/design.js
+++ design-assets/design.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
+	// Selectors for main divs - game, recipe search, meal history
+	const gameDiv = $('div#game-div');
+	const startBtn = $('button#start-btn');
 
-    // Selectors for main divs - game, recipe search, meal history
-    const gameDiv = $('div#game-div');
-    const startBtn = $('button#start-btn');
+	// for testing only - hide and show different divs
+	const hideGameBtn = $('#hide-game');
 
-    // for testing only - hide and show different divs
-    const hideGameBtn = $('#hide-game');
+	function hideShow(element) {
+		element.toggleClass('hide');
+	}
 
-    function hideShow(element) {
-        element.toggleClass("hide");
-    }
+	hideGameBtn.on('click', function() {
+		hideShow(gameDiv);
+	});
 
-    hideGameBtn.on("click", function () {
-        hideShow(gameDiv);
-    })
+	// Materialize Side Nav Menu for Mobile
+	$('.sidenav').sidenav();
 
-    // Materialize Side Nav Menu for Mobile
-    $('.sidenav').sidenav();
+	// Materialize Initialization for Modal
+	$('.modal').modal();
 
-    // Materialize Initialization for Modal
-    $('.modal').modal();
-
-
+	// Materialize Initialization for Select
+	$('select').formSelect();
 });
