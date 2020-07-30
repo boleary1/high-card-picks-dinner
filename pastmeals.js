72d639ce6245bd73e5a8d02ba0bb56e0d7331532
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-07-29 21:41:05 -0400

    starting to add correct classes from html design

--- pastmeals.js
+++ pastmeals.js
@@ -1,17 +1,9 @@
 $(document).ready(function() {
     // The div that will hold the populated images, past winners and dates
-    const historyContainer = $('div.history-container');
-
-    const categoryEl = $('#icon_category');
-    // const searchEl = $('icon_search');
-    const searchBtn = $('button.search');
-    const nameEl = $('input#name')
+    const historyContainer = $('div.history-div');
 
     // array to be used to store the information which will be saves in local storage
     let historyData = [];
-
-
-    $('select').formSelect(); //From materialize - not sure if i need this.
 
     // Gets info from local storage and stores it in the historyData variable if there is any available.
     function getHistory() {
@@ -23,13 +15,13 @@ $(document).ready(function() {
         // If history data exists, create a card for each object and add the img, meal name, who picked it, and date
         if (historyData[0].Name) {
             historyData.forEach(function(object){
-                const historyDiv = $('<div>').addClass('history col xs12 s6 m4 l2');
+                const historyDiv = $('<div>').addClass('col s12 m3');
                 const historyCard = $('<div>').addClass('card');
                 const cardImgDiv = $('<div>').addClass('card-img');
                // const aLink = $('<a>').attr('href', object.RecipeLink).attr('target', '_blank');
-                const img = $('<img>').attr('src', object.ImgURL).attr('alt', object.Meal).attr('class', 'imgLink').width('100%');
-                const textDiv = $('<div>').addClass('history-text card-content');
-                const mealName = $('<p>').addClass('meal-name card-title').text(object.Meal);
+                const img = $('<img>').attr('src', object.ImgURL).attr('alt', object.Meal).attr('id', 'past-recipe-img');
+                const textDiv = $('<div>').addClass('card-content');
+                const mealName = $('<p>').addClass('card-title').attr('id', 'past-recipe-name').text(object.Meal);
                 const whoPicked = $('<p>').addClass('picked-by').text(`${object.Name} picked on:`);
                 const date = $('<p>').addClass('date-picked').text(`${object.Date}`);
 
@@ -101,9 +93,10 @@ $(document).ready(function() {
         searchCategory(categoryEl.val());
     });
 
-    $("img.imgLink").on("click", function() {
-        
-    });
+    // $("img.imgLink").on("click", function() {
+    //     console.log($(this).attr('data-index'));
+    //     // location.href=
+    // });
 
 
 
