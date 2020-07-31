c10d9ebca04a3422522c540a7954ec610f93fcb0
Brian OLeary <oleary.brian1@gmail.com>
2020-07-31 14:40:20 -0400

    Displays recipe options inside a category

--- design-assets/design.js
+++ design-assets/design.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	const startBtn = $('button#start-btn');
 	const searchDiv = $('div#search-div');
 	let selectedCategory;
-	// var recipeSelection = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + selectedCategory;
+	const optionsContainer = $('div.options-container');
 
 	const pastDiv = $('div#past-div');
 
@@ -17,25 +17,49 @@ $(document).ready(function () {
 		element.toggleClass('hide');
 	}
 
+
 	$('#search-btn').on('click', function () {
 		event.preventDefault();
-		console.log($('#meal-category').val());
 		selectedCategory = $('#meal-category').val();
-		console.log("selected category " + selectedCategory);
 		$.ajax({ // this ajax call will display recipes in a selected cattagory
 			url: "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + selectedCategory,
 			method: "GET",
 		})
 			.then(function (responseRecipeSelection) {
-				// console.log(responserecipeSelection)
 				var b = responseRecipeSelection.meals
-				// console.log(b)
 				b.forEach(function (displayoptions) {
-					$(".cityCurrent").html(); //add city and date
 
-					// console.log(displayoptions.idMeal);
-					console.log(displayoptions.strMeal);
-					// console.log(displayoptions.strMealThumb);
+					const mealId = displayoptions.idMeal
+					let mealLink = ""
+
+					const mealRecipe = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId;
+
+					$.ajax({ //this ajax call gets the URL for the recipe I wrote the HTML inside this, to get the URL before I write the HTML
+						url: mealRecipe,
+						method: "GET",
+					})
+						.then(function (mealDisplay) {
+							mealLink = mealDisplay.meals[0].strSource
+							const optionsDiv = $('<div>').addClass('options col s12 m3');
+							const optionsCard = $('<div>').addClass('card');
+							const optionsCardImgDiv = $('<div>').addClass('card-imgage');
+							const optionsImg = $('<img>').attr('src', displayoptions.strMealThumb).attr('alt', displayoptions.strMeal).width('100%');
+							const addButton = $('<a>').addClass('btn-floating halfway-fab waves-effect waves-light red');
+							const addIcon = $('<i>').addClass('material-icons').text('add');
+							const optionsTextDiv = $('<div>').addClass('history-text card-content');
+							const optionsMealName = $('<span>').addClass('meal-name card-title').text(displayoptions.strMeal);
+							const recipeLinkDiv = $('<div>').addClass('card-action');
+							const addLinkButton = $('<a>').attr("href", mealLink).text('View Recipe');
+
+							optionsTextDiv.append(optionsMealName);
+							addButton.append(addIcon)
+							optionsCardImgDiv.append(optionsImg, addButton);
+							recipeLinkDiv.append(addLinkButton)
+							optionsCard.append(optionsCardImgDiv, optionsTextDiv, recipeLinkDiv);
+							optionsDiv.append(optionsCard);
+							optionsContainer.append(optionsDiv);
+						});
+
 
 				});
 
@@ -52,7 +76,7 @@ $(document).ready(function () {
 		hideShow(searchDiv);
 	});
 
-	showPastBtn.on('click', function() {
+	showPastBtn.on('click', function () {
 		hideShow(pastDiv);
 	});
 
